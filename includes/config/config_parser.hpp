7de630b9cc105a55e0635d2c009312c24dbcacc9
mboutahir <ssmomo94@gmail.com>
2026-01-27 17:20:14 +0100

    starting config_parse logic

--- includes/config/config_parser.hpp
+++ includes/config/config_parser.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <map>
 #include <iostream>
+#include <ifstream>
 class config_parser {
 
     public:
         std::vector<std::string> vect;
         config_parser(const char* file_path);
     };
-    void parse(const char* file_path);
+    bool parse(const char* file_path, std::vector<std::string> &vect);
+    bool tokenize(std::ifstream &file, std::vector<std::string> &vect);
+    bool has_only_space(std::string s);
--- srcs/config/config_parser.cpp
+++ srcs/config/config_parser.cpp
@@ -1,10 +1,46 @@
 #include "../../includes/config/config_parser.hpp"
 
+  bool has_only_space(std::string s)
+  {
+    unsigned int size = s.size();
+    for (unsigned int i = 0 ; i< size; i++)
+    {
+        if(s[i] != '\t' || s[i] != '\n' || s[i] != '\v' || s[i] != '\f' || s[i] != '\r')
+            return false;
+    }
+    return true;
+  }
 
+ bool tokenize(std::ifstream &file, std::vector<std::string> &vect)
+ {
+    std::string s;
+    while(std::getline(file, s))
+    {
+        if(s.empty() || has_only_space(s) == true)
+            continue;
+
+    }
+    if(vc.empty())
+        return false;
+
+ }
+
+bool parse(const char* file_path, std::vector<std::string> &vect)
+{
+    std::ifstream file(file_path);
+    if(!file.is_open())
+    {
+        std::cerr<<"Error: could not open file"<<std::endl;
+        retun false;
+    }
+    if(tokenize(&file, &vect) == false)
+        return false;
+
+}
 
 config_parser::config_parser(const char* s)
 {
-    
-    parse(s, vect);
+    if(parse(s, &vect) == false)
+        return ;
 
 }
