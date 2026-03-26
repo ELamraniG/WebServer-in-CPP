fc9b0ab71e5d94cf5537586bc10dffef0d653e39
Reda Daghouj <mohamedredadaghouj@gmail.com>
2026-03-26 07:20:02 +0100

    refactor: enhance socket handling and improve server port management

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 
 #include <cstdio>
 #include <cstring>
+#include <map>
 #include <netinet/in.h>
 #include <iostream>
+#include <string>
 #include <sys/poll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <vector>
+#include <set>
 #include <poll.h>
+#include <fcntl.h>
 
-#define PORT 8080
 #define BUFFER_SIZE 1024
+#define TIMEOUT 1000
+
+std::string response("HTTP/1.0 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+
+void cleanup(std::map<int, std::string> &receivers, std::map<int, std::string> &senders, std::vector<pollfd> &fds, int &i)
+{
+	close(fds[i].fd);
+	receivers.erase(fds[i].fd);
+	senders.erase(fds[i].fd);
+	fds.erase(fds.begin() + i);
+	i--;
+}
 
 int error(const char *msg)
 {
@@ -29,101 +44,133 @@ int main()
 	socklen_t client_len = sizeof(client_addr);
 	int opt = 1;
 
-	// create socket
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
-		return (error("socket"));
-	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-	// setup server address
-	std::memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(PORT);
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-
-	// bind server to an address
-	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	{
-		close(server_fd);
-		return (error("bind"));
-	}
-
-	// listen
-	if (listen(server_fd, 10) < 0)
-	{
-		close(server_fd);
-		return (error("listen"));
-	}
-
-	std::cout << "Server listening on port " << PORT << std::endl;
-
-	// read from client socket
-	std::string response("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
-	std::string receiver;
+	std::vector<int> ports;
+	ports.push_back(8080);
+	ports.push_back(8081);
+	ports.push_back(8082);
 	std::vector<pollfd>fds;
+	std::set<int>server_fds;
 	pollfd server_poll;
-	server_poll.fd = server_fd;
-	server_poll.events = POLLIN;
-	fds.push_back(server_poll);
+	for (int i=0; i<ports.size(); i++)
+	{
+		server_fd = socket(AF_INET, SOCK_STREAM, 0);
+		if (server_fd < 0)
+			return (error("socket"));
+		if (fcntl(server_fd, F_SETFL, O_NONBLOCK) == -1)
+		{
+			close(server_fd);
+			return (error("fcntl(F_SETFL)"));
+		}
+		setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	
+		std::memset(&server_addr, 0, sizeof(server_addr));
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_port = htons(ports[i]);
+		server_addr.sin_addr.s_addr = INADDR_ANY;
+	
+		if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+		{
+			close(server_fd);
+			return (error("bind"));
+		}
+	
+		if (listen(server_fd, 10) < 0)
+		{
+			close(server_fd);
+			return (error("listen"));
+		}
+		server_fds.insert(server_fd);
+		server_poll.fd = server_fd;
+		server_poll.events = POLLIN;
+		fds.push_back(server_poll);
+	}
+
+	std::cout << "Server listening on ports: " << ports[0] << ", " << ports[1] << ", " << ports[2] << std::endl;
+
+	std::map<int, std::string> receivers;
+	std::map<int, std::string> senders;
 	while (true)
 	{
 		int ret = poll(fds.data(), fds.size(), -1);
 		if (ret < 0)
 		{
-			close(server_fd);
+			for (int i=0; i<fds.size(); i++)
+				close(fds[i].fd);
 			return (error("poll"));
 		}
 		for (int i=0; i<fds.size(); i++)
 		{
-			if (fds[i].revents & POLLIN)
+			if (!server_fds.count(fds[i].fd) && (fds[i].revents & (POLLERR | POLLHUP)))
+				cleanup(receivers, senders, fds, i);
+			else if (fds[i].revents & POLLIN)
 			{
-				if (fds[i].fd == server_fd)
+				if (server_fds.count(fds[i].fd))
 				{
-					// accept
-					client_fd = accept(server_fd,  (struct sockaddr *)&client_addr, &client_len);
+					client_fd = accept(fds[i].fd,  (struct sockaddr *)&client_addr, &client_len);
 					if (client_fd < 0)
 					{
-						close(server_fd);
-						return (error("accept"));
+						perror("accept");
+						continue ;
 					}
-					receiver.clear();
+					else if (fcntl(client_fd, F_SETFL, O_NONBLOCK) == -1)
+					{
+						perror("fcntl(F_SETFL)");
+						close(client_fd);
+						continue ;
+					}	
 					pollfd client_poll;
 					client_poll.fd = client_fd;
 					client_poll.events = POLLIN;
 					fds.push_back(client_poll);
+					receivers[client_fd] = "";
 					std::cout << "Client connect" << std::endl;
 				}
 				else
 				{
-					while (true)
+					int bytes = read(fds[i].fd, &buffer[0], BUFFER_SIZE);
+					if (bytes > 0)
 					{
-						int bytes = read(fds[i].fd, &buffer[0], BUFFER_SIZE);
-						if (bytes > 0)
+						receivers[fds[i].fd].append(buffer.c_str(), bytes);
+						if (receivers[fds[i].fd].find("\r\n\r\n") != std::string::npos)
 						{
-							receiver.append(buffer.c_str(), bytes);
-							if (receiver.find("\r\n\r\n") != std::string::npos)
-								break ;
-						}
-						else if (bytes == 0)
-						{
-							std::cout << "Client disconnected" << std::endl;
-							break ;
-						}
-						else
-						{
-							close(server_fd);
-							close(fds[i].fd);
-							return (error("read"));
+							fds[i].events = POLLOUT;
+							senders[fds[i].fd] = response;
+							// write(fds[i].fd, response.c_str(), response.size());
+							// cleanup(receivers, fds, i);
 						}
 					}
-					write(fds[i].fd, response.c_str(), response.size());
-					close(fds[i].fd);
-					fds.erase(fds.begin() + i);
-					i--;
+					else if (bytes == 0)
+					{
+						std::cout << "Client disconnected" << std::endl;
+						cleanup(receivers, senders, fds, i);
+					}
+					else
+					{
+						perror("read");
+						cleanup(receivers, senders, fds, i);
+					}
 				}
+			}
+			else if (fds[i].revents & POLLOUT)
+			{
+				int response_size = senders[fds[i].fd].size();
+				int bytes = write(fds[i].fd, senders[fds[i].fd].c_str(), response_size);
+				if (bytes < 0)
+				{
+					perror("write");
+					cleanup(receivers, senders, fds, i);
+				}
+				else if (bytes == 0)
+				{
+					std::cout << "Client disconnected" << std::endl;
+					cleanup(receivers, senders, fds, i);
+				}
+				else if (bytes == response_size)
+					cleanup(receivers, senders, fds, i);
+				else
+					senders[fds[i].fd].erase(0, bytes);
 			}
 		}
 	}
-	close(server_fd);
 	return (0);
 }
